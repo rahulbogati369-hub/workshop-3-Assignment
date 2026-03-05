2e8fb576c249ad868b35ad5889e42231b2eeeff7
rahulbogati369-hub <rahulbogati369@gmail.com>
2026-03-05 14:40:43 +0545

    Add files via upload

--- MathOperations.java
+++ MathOperations.java
@@ -1,11 +1,13 @@
 public class MathOperations {
     public static void main(String[] args) {
         
-        int age = 20;
-        String result;
-        result = (age >= 18) ? "Adult" : "Minor";
-
-        System.out.println("\nQuestion 6: Ternary Operator");
-        System.out.println("Age status: " + result);
+        boolean p = true, q = false;
+        System.out.println("\nLogical Operators:");
+        System.out.println("p && q = " + (p && q));
+        System.out.println("p || q = " + (p || q));
+        System.out.println("!p = " + (!p));
+        
     }
+    
 }
+
